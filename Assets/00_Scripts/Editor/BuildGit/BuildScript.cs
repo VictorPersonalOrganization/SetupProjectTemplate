93514451fc379fe171435e327554afc763fd2f52
Victor Baz <v.baz@rubika-edu.com>
2026-08-02 17:50:13 +0200

    [ADD] BuildScript to help git hub action

---
+++ Assets/00_Scripts/Editor/BuildGit/BuildScript.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public static class BuildScript
+{
+    private static string[] Scenes =>
+        EditorBuildSettings.scenes
+            .Where(s => s.enabled)
+            .Select(s => s.path)
+            .ToArray();
+
+    public static void BuildWindows()
+    {
+        string buildDir = GetArg("-customBuildDir") ?? "Builds/Windows";
+        string exeName = GetArg("-customExeName") ?? "Game.exe";
+        string outputPath = $"{buildDir}/{exeName}";
+
+        BuildReport report = BuildPipeline.BuildPlayer(
+            Scenes,
+            outputPath,
+            BuildTarget.StandaloneWindows64,
+            BuildOptions.None
+        );
+
+        if (report.summary.result != BuildResult.Succeeded)
+        {
+            throw new Exception($"Build failed : {report.summary.result}");
+        }
+    }
+
+    private static string GetArg(string name)
+    {
+        string[] args = Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length - 1; i++)
+        {
+            if (args[i] == name)
+            {
+                return args[i + 1];
+            }
+        }
+        return null;
+    }
+}
